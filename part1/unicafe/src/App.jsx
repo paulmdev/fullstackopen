cc2fd01f6a87252dcd487a5486e308204b951f30
paulmdev <paulmdev@protonmail.com>
2022-03-11 17:58:10 -0500

    🎉 Start unicafe exercise

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <main>
+      <h2>Give feedback</h2>
+      <button>Good</button>
+      <button>Neutral</button>
+      <button>Bad</button>
+      <h2>Stadistics</h2>
+      <p>good 6</p>
+      <p>neutral 2</p>
+      <p>bad 1</p>
+    </main>
+  );
+}
+
+export default App;
