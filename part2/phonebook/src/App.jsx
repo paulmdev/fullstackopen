8256dbf8092e3a70eddded55a0b94addb0523d8b
paulmdev <paulmdev@protonmail.com>
2022-03-15 18:43:42 -0500

    ✨ Finish step1

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,19 +4,36 @@ const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
 
+  const handleSubmitEvent = (event) => {
+    event.preventDefault();
+
+    const phonebookEntry = {
+      name: newName,
+    };
+
+    setPersons([...persons, phonebookEntry]);
+    setNewName('');
+  };
+
+  const handleNameChange = (event) => setNewName(event.target.value);
+
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={handleSubmitEvent}>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} value={newName} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map((person) => (
+          <li key={person.name}>{person.name}</li>
+        ))}
+      </ul>
     </div>
   );
 };
