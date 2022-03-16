ea569080e916aeade3782f9bcb7e2e5453467c2a
paulmdev <paulmdev@protonmail.com>
2022-03-15 19:16:14 -0500

    ✨ Finish step3

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -3,6 +3,7 @@ import { useState } from 'react';
 const App = () => {
   const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
   const [newName, setNewName] = useState('');
+  const [newNumber, setNewNumber] = useState('');
 
   const handleSubmitEvent = (event) => {
     event.preventDefault();
@@ -14,16 +15,19 @@ const App = () => {
 
     const phonebookEntry = {
       name: newName,
+      number: newNumber
     };
 
     setPersons([...persons, phonebookEntry]);
     setNewName('');
+    setNewNumber('');
   };
 
   const isNameDuplicated = () =>
     persons.find((person) => person.name == newName);
 
   const handleNameChange = (event) => setNewName(event.target.value);
+  const handleNumberChange = (event) => setNewNumber(event.target.value);
 
   return (
     <div>
@@ -33,13 +37,16 @@ const App = () => {
           name: <input onChange={handleNameChange} value={newName} />
         </div>
         <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
+        </div>
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>{person.name}</li>
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
