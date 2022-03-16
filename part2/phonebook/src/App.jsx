5a4d8f891912e66d0f463b217e99a942434fdeaa
paulmdev <paulmdev@protonmail.com>
2022-03-15 19:30:01 -0500

    ✨ Finish Part4

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,9 +1,15 @@
 import { useState } from 'react';
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]);
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ]);
   const [newName, setNewName] = useState('');
   const [newNumber, setNewNumber] = useState('');
+  const [filter, setFilter] = useState('');
 
   const handleSubmitEvent = (event) => {
     event.preventDefault();
@@ -15,7 +21,7 @@ const App = () => {
 
     const phonebookEntry = {
       name: newName,
-      number: newNumber
+      number: newNumber,
     };
 
     setPersons([...persons, phonebookEntry]);
@@ -29,9 +35,17 @@ const App = () => {
   const handleNameChange = (event) => setNewName(event.target.value);
   const handleNumberChange = (event) => setNewNumber(event.target.value);
 
+  const handleFilterChange = (event) =>
+    setFilter(event.target.value.toLowerCase());
+
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>
+        filter shown with{' '}
+        <input onChange={handleFilterChange} value={filter} type="text" />{' '}
+      </div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmitEvent}>
         <div>
           name: <input onChange={handleNameChange} value={newName} />
@@ -45,9 +59,13 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map((person) => (
-          <li key={person.name}>{person.name} {person.number}</li>
-        ))}
+        {persons
+          .filter((person) => person.name.toLowerCase().includes(filter))
+          .map((person) => (
+            <li key={person.name}>
+              {person.name} {person.number}
+            </li>
+          ))}
       </ul>
     </div>
   );
