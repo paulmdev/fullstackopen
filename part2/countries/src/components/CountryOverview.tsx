4f63d29b998d94f923c9f43e143fc15d9c9980c7
paulmdev <paulmdev@protonmail.com>
2022-03-16 22:26:37 -0500

    ✨ Finish step3

--- part2/countries/src/components/CountryOverview.tsx
+++ part2/countries/src/components/CountryOverview.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { RestCountriesResponse } from "../types/RestCountriesResponse";
+import axios from "axios";
+
+interface WeatherResponse {
+  weather: { id: number; main: string; description: string; icon: string }[];
+  main: { temp: number };
+  wind: { speed: number };
+}
 
 const CountryOverview: React.FC<{ country: RestCountriesResponse }> = ({
   country: {
@@ -10,6 +17,16 @@ const CountryOverview: React.FC<{ country: RestCountriesResponse }> = ({
     name: { common },
   },
 }) => {
+  const [weather, setWeather] = useState<WeatherResponse>();
+
+  useEffect(() => {
+    axios
+      .get<WeatherResponse>(`http://api.openweathermap.org/data/2.5/weather`, {
+        params: { q: capital[0], appid: import.meta.env.VITE_OWM_API_KEY },
+      })
+      .then((response) => setWeather(response.data));
+  }, []);
+
   return (
     <>
       <h1>{common}</h1>
@@ -22,6 +39,13 @@ const CountryOverview: React.FC<{ country: RestCountriesResponse }> = ({
         ))}
       </ul>
       <img src={flags.png} alt="Country flag" />
+      <h3>Weather in {capital}</h3>
+      <p>temperature {weather?.main.temp} Celsius</p>
+      <img
+        src={`http://openweathermap.org/img/wn/${weather?.weather[0].icon}@2x.png`}
+        alt="Weather icon"
+      />
+      <p>Wind {weather?.wind.speed} m/s</p>
     </>
   );
 };
