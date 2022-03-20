a74c5d7e017fc7f78814775356498688068b410f
paulmdev <paulmdev@protonmail.com>
2022-03-20 16:45:25 -0500

    :sparkles: Finish step1

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,35 @@
-import Express from "express";
+const Express = require("express");
 
 const app = Express();
 
 app.use(Express.json());
+
+const data = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
+app.get("/api/persons", (req, res) => {
+  return res.status(200).send(data).end();
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
