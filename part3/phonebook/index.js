9cb6b5e0f69ebf7c35ad837d10e5fdd913b07197
paulmdev <paulmdev@protonmail.com>
2022-03-20 16:54:32 -0500

    :sparkles: Finish step2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -6,30 +6,40 @@ app.use(Express.json());
 
 const data = [
   {
-    "id": 1,
-    "name": "Arto Hellas",
-    "number": "040-123456"
+    id: 1,
+    name: "Arto Hellas",
+    number: "040-123456",
   },
   {
-    "id": 2,
-    "name": "Ada Lovelace",
-    "number": "39-44-5323523"
+    id: 2,
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
   },
   {
-    "id": 3,
-    "name": "Dan Abramov",
-    "number": "12-43-234345"
+    id: 3,
+    name: "Dan Abramov",
+    number: "12-43-234345",
   },
   {
-    "id": 4,
-    "name": "Mary Poppendieck",
-    "number": "39-23-6423122"
-  }
-]
+    id: 4,
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
+
+app.get("/info", (req, res) => {
+  const phonebookInfo = `
+    <p>The phonebook has info for ${data.length} people</p>
+    <p>${new Date()}</p>
+`;
+  return res.status(200).send(phonebookInfo).end();
+});
 
 app.get("/api/persons", (req, res) => {
   return res.status(200).send(data).end();
-})
+});
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => console.log(`Server running in http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, () =>
+  console.log(`Server running in http://localhost:${PORT}`)
+);
