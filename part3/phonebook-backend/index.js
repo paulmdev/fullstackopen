9365ab8a836c6b6f93befee13f865763ba323ea1
paulmdev <paulmdev@protonmail.com>
2022-03-22 10:38:23 -0500

    :sparkles: Finish 3.10 step10
    Deploy the app to Heroku
    
    Signed-off-by: paulmdev <paulmdev@protonmail.com>

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -87,6 +87,4 @@ app.delete("/api/persons/:id", (req, res) => {
 });
 
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () =>
-  console.log(`Server running in http://localhost:${PORT}`)
-);
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
