fffac9ef8d067b76da4afc5d076d77e76a2df522
paulmdev <paulmdev@protonmail.com>
2022-03-26 15:27:28 -0500

    ✨ Finish exercise 3.22

--- part3/phonebook-backend/models/Person.js
+++ part3/phonebook-backend/models/Person.js
@@ -5,20 +5,20 @@ const url = process.env.MONGODB_URI;
 console.log("Connecting to MongoDB Atlas...");
 
 mongoose
-    .connect(url)
-    .then(() => console.log("Connected to the database!"))
-    .catch((error) =>
-        console.error("An error occurred when connecting to the database: ", error)
-    );
+  .connect(url)
+  .then(() => console.log("Connected to the database!"))
+  .catch((error) =>
+    console.error("An error occurred when connecting to the database: ", error)
+  );
 
 const personSchema = new mongoose.Schema({ name: String, number: String });
 
 personSchema.set("toJSON", {
-    transform: (doc, ret) => {
-        ret.id = ret._id.toString();
-        delete ret._id;
-        delete ret.__v;
-    },
+  transform: (doc, ret) => {
+    ret.id = ret._id.toString();
+    delete ret._id;
+    delete ret.__v;
+  },
 });
 
 module.exports = mongoose.model("Person", personSchema);
