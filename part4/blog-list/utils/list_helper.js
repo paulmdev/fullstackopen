eef27736f00d572bc7c4bd38dd53daf2312a186a
paulmdev <paulmdev@protonmail.com>
2022-03-27 13:11:54 -0500

    :sparkles: Finish exercises 4.3 to 4.7
    - Create utility functions.
    - Test each utility function

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,42 @@
+const lodash = require('lodash');
+
 const dummy = (blogs) => 1;
 
-module.exports = { dummy };
+const totalLikes = (blogs) => {
+  const reducer = (likes, blog) => blog.likes + likes;
+  return blogs.length === 0 ? 0 : blogs.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) return undefined;
+
+  const maximumLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const { likes, title, author } = blogs.find((blog) => blog.likes === maximumLikes);
+  return { likes, title, author };
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length === 0) return undefined;
+
+  const countBlogsByAuthor = lodash.countBy(blogs, 'author');
+
+  const blogsByAuthor = Object.entries(countBlogsByAuthor).map((entry) => (
+    { author: entry[0], blogs: entry[1] }
+  ));
+
+  return lodash.maxBy(blogsByAuthor, 'blogs');
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length === 0) return undefined;
+
+  const groupByAuthor = lodash.groupBy(blogs, 'author');
+
+  const aggregatedLikes = Object.entries(groupByAuthor).map((entry) => ({ author: entry[0], likes: lodash.sumBy(entry[1], 'likes') }));
+
+  return lodash.maxBy(aggregatedLikes, 'likes');
+};
+
+module.exports = {
+  dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes,
+};
