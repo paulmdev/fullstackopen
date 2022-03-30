a3dbb0032ecf142ffc8f560e33f866b7868bbb2c
paulmdev <paulmdev@protonmail.com>
2022-03-30 10:44:10 -0500

    :goal_net: Use error handling on the routes

--- part4/blog-list/controller/blogs.js
+++ part4/blog-list/controller/blogs.js
@@ -1,16 +1,23 @@
 const router = require("express").Router();
 const Blog = require("../models/blog");
 
-router.get("/", async (request, response) => {
-  const blogs = await Blog.find({});
-  return response.json(blogs);
+router.get("/", async (request, response, next) => {
+  try {
+    const blogs = await Blog.find({});
+    return response.json(blogs);
+  } catch (error) {
+    next(error);
+  }
 });
 
-router.post("/", async (request, response) => {
-  const blog = new Blog(request.body);
-
-  const result = await blog.save();
-  response.status(201).json(result);
+router.post("/", async (request, response, next) => {
+  try {
+    const blog = new Blog(request.body);
+    const result = await blog.save();
+    response.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 module.exports = router;
