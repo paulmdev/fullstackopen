0884587c03c816bbd7bcb089c869f60f1de717f5
paulmdev <paulmdev@protonmail.com>
2022-03-29 21:03:01 -0500

    :recycle: Create node environments

--- part4/blog-list/utils/config.js
+++ part4/blog-list/utils/config.js
@@ -1,5 +1,10 @@
 require("dotenv").config();
 
-const { PORT, MONGODB_URI } = process.env;
+const { PORT } = process.env;
+
+const MONGODB_URI =
+  process.env.NODE_ENV === "test"
+    ? process.env.MONGODB_TEST_URI
+    : process.env.MONGODB_URI;
 
 module.exports = { PORT, MONGODB_URI };
