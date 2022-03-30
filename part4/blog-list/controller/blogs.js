19b17d930915ec8092e601cea8d8b86543e22023
paulmdev <paulmdev@protonmail.com>
2022-03-30 11:34:56 -0500

    :sparkles: Implement delete and put route

--- part4/blog-list/controller/blogs.js
+++ part4/blog-list/controller/blogs.js
@@ -20,4 +20,32 @@ router.post("/", async (request, response, next) => {
   }
 });
 
+router.delete("/:id", async (req, res, next) => {
+  try {
+    await Blog.findByIdAndDelete(req.params.id);
+    return res.sendStatus(204);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    if (req.body.likes === undefined) return res.sendStatus(400);
+
+    const { likes } = req.body;
+
+    const updatedBlog = await Blog.findByIdAndUpdate(
+      req.params.id,
+      { likes },
+      {
+        new: true,
+      }
+    );
+    return res.json(updatedBlog);
+  } catch (error) {
+    next(error);
+  }
+});
+
 module.exports = router;
