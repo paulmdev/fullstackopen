9439f430a66b49022f3f9aeb7ea0491761fa699c
paulmdev <paulmdev@protonmail.com>
2022-03-30 11:02:25 -0500

    :truck: Move listWithOneBlog constant to test_helpers.js

--- part4/blog-list/tests/list_helper.test.js
+++ part4/blog-list/tests/list_helper.test.js
@@ -6,24 +6,13 @@ const {
 } = require("../utils/list_helper");
 const helpers = require("./test_helpers");
 
-const listWithOneBlog = [
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0,
-  },
-];
-
 describe("total likes", () => {
   test("of empty list is 0", () => {
     expect(totalLikes([])).toBe(0);
   });
 
   test("when list has only one blog is equals to that blog's likes", () => {
-    expect(totalLikes(listWithOneBlog)).toBe(5);
+    expect(totalLikes(helpers.listWithOneBlog)).toBe(5);
   });
 
   test("when the list is bigger than one blog", () => {
@@ -37,7 +26,7 @@ describe("favorite blog", () => {
   });
 
   test("when the list has only one blog", () => {
-    expect(favoriteBlog(listWithOneBlog)).toStrictEqual({
+    expect(favoriteBlog(helpers.listWithOneBlog)).toStrictEqual({
       title: "Go To Statement Considered Harmful",
       author: "Edsger W. Dijkstra",
       likes: 5,
--- part4/blog-list/tests/test_helpers.js
+++ part4/blog-list/tests/test_helpers.js
@@ -1,3 +1,14 @@
+const listWithOneBlog = [
+  {
+    _id: "5a422aa71b54a676234d17f8",
+    title: "Go To Statement Considered Harmful",
+    author: "Edsger W. Dijkstra",
+    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
+    likes: 5,
+    __v: 0,
+  },
+];
+
 const blogs = [
   {
     _id: "5a422a851b54a676234d17f7",
@@ -49,4 +60,4 @@ const blogs = [
   },
 ];
 
-module.exports = { blogs };
+module.exports = { blogs, listWithOneBlog };
