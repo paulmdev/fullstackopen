eaff46654f3f5f9a777ec25ab570d2a1c04e79fa
paulmdev <paulmdev@protonmail.com>
2022-03-29 21:25:18 -0500

    :recycle: Use async/await instead of promises in the GET handler

--- part4/blog-list/controller/blogs.js
+++ part4/blog-list/controller/blogs.js
@@ -1,22 +1,17 @@
 const router = require("express").Router();
 const Blog = require("../models/blog");
 
-router.get("/", (request, response) => {
-  Blog
-    .find({})
-    .then((blogs) => {
-      response.json(blogs);
-    });
+router.get("/", async (request, response) => {
+  const blogs = await Blog.find({});
+  return response.json(blogs);
 });
 
 router.post("/", (request, response) => {
   const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then((result) => {
-      response.status(201).json(result);
-    });
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
 });
 
 module.exports = router;
